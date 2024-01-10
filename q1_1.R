f2d93da0fcd8de0e8190b30d7b95672467fba134
Caden Hewlett <cadenh139@edu.sd45.bc.ca>
2024-01-09 20:07:11 -0800

    pretty much done

--- q1_1.R
+++ q1_1.R
@@ -56,3 +56,29 @@ posterior_given_four_heads((1:10))
 # we might say prop to because it is prop to some continuous distribution, i.e.
 
   
+### question 5
+posterior <- function(rho, n_heads, n_observations){
+  # this part is the same
+  kvals = 0:(length(rho)-1)
+  K = length(kvals)
+  biases = kvals / (K-1)
+  # we need the overall probability of nheads
+  p_E =  sum(dbinom(x = n_heads, size = n_observations, p = biases)*rho)
+  # then, across the k coins that can be picked
+  # then numerator is still P(X)P(E | X)
+  prior_likeli =  sapply(1:K, function(k){ 
+    dbinom(x = n_heads, size = n_observations, p = biases[k]) * rho[k]} ) 
+  # then return the ratio
+  return(prior_likeli / p_E)
+}
+
+posterior(c(1/3, 1/3, 1/3), 4, 4)
+
+
+# Show the output for rho propto some shit and n_heads = 2 and n_observations = 10.
+
+plot(posterior(1:10, 2, 10))
+
+# plot(posterior(1:10, 2, 10))
+
+posterior ( 1:10/(sum(1:10)), 2, 10)
