d052943e1217703c05e12961a8099fc7cfc819ee
Caden Hewlett <thetroutmanishere@gmail.com>
2024-03-08 11:37:05 -0800

    q-learn implementation in OLE

--- project_outline.R
+++ project_outline.R
@@ -1,8 +1,12 @@
-epsilon = 0.0
-alpha = 0.0
-gamma = 0.0
-x = y = 1
+epsilon = 0.2
+alpha = 0.5
+gamma = 0.5
+
+x = y = x_old = y_old = 3
+
 library(pracma)
+library(extraDistr)
+## Define the obstacle course
 course = matrix(
   c(0, 1, 1,
     0, 1, 1,
@@ -10,6 +14,10 @@ course = matrix(
   nrow = 3, ncol = 3,
   byrow = T
 )
+## set the starting position
+course[x, y] = 2
+
+## set the Optimal Learning Envrionment (OLE) move order
 order = matrix(
   c(5, 0, 0,
     4, 0, 0,
@@ -17,35 +25,81 @@ order = matrix(
   nrow = 3, ncol = 3,
   byrow = T
 )
+
+# Create an OLE Rewards Schema
 R = apply(order, MARGIN = c(1,2), 
       FUN = function(cell){
         runif(1, min = -1 + cell, max = cell)
       })
-# pad for out of bounds movement
-PAD <- matrix(-1, nrow = 5, ncol = 5)
+
+# Create Pad for out of bounds movement
+PAD <- matrix(-2, nrow = 5, ncol = 5)
 PAD[2:4, 2:4] <- R
+# Padded rewards matrix penalizes moving out of bounds
 R <- PAD
 
-set_start = function(x, y){
-  course[x, y] <<- 2
-}
-
+# Define Action Set
+A = list(
+  UP <- function(){x <<- x - 1; },
+  DOWN <- function(){x <<- x + 1; },
+  RIGHT <- function(){y <<- y + 1;},
+  LEFT <- function(){y <<- y - 1; }
+)
+VERBOSE = c("UP", "DOWN", "RIGHT", "LEFT")
+# Initialize random uniform Q-Table
+Q = array(runif(3*3*4, min = 0, max = 0.05), dim = c(3, 3, 4))
+r_t = 0
+# Attempt Movement at Time t
+while( FALSE ) { #sum(c(x, y) == c(1, 1)) <= 2
 tryCatch({
-  cat("Selected action... \n")
-  course[1, 4] = 2
+  # Epsilon-Greedy
+  if(rbern(1, epsilon) == 1){
+    print("Exploring...")
+    a = rdunif(1, min = 1, max = 4)
+  } else {
+    print("Exploiting...")
+    a = which.max( Q[x, y,] )
+  }
+  # Save Previous Position
+  x_t = x; y_t = y;
+  # Set old spot to zero
+  course[x_t, y_t] = 0
+  print(
+    paste("Selected action:", VERBOSE[a] )
+  )
   # perform epsilon-greedy and select action
   cat("Moving... \n")
+  A[[a]]()
+  print(paste("New Position:", x, y))
   # attempt to update location from new movement
-  # ...
+  course[x, y] = 2
+  # fetch reward
+  r_t =  R[x,y]
+  print(paste("Reward:", r_t))
 }, error = function(e) {
   if(grepl("subscript out of bounds", e$message)) {
     cat("Agent attempted to move out of bounds. \n")
     # agent stays in the same location
-    # assign static penalty (alternatively, expand R and index it)
+    print(c(x, y))
+    r_t <<- R[x, y]
+    x <<- x_t
+    y <<- y_t
+    print(paste("New Position:",x, y))
+    print(paste("Reward:", r_t))
   } 
 }, finally = {
-  cat("Updating Q(s,a)...")
+  course[x, y] = 2
+  cat("Updating Q(x,y,a)...\n")
+  print( paste("Old Q-Score:", Q[x_t, y_t, a]) )
+  # Q-Table Update
+  Q[x_t, y_t, a] = Q[x_t, y_t, a] + alpha*(
+    r_t + gamma*Q[x, y, which.max( Q[x, y, ])] 
+    - Q[x_t, y_t, a])
+  print( paste("New Q-Score:", Q[x_t, y_t, a]) )
 })
-
-
-
+Sys.sleep(1)
+}
+course
+# TODO: create offset schema for R indexing
+R[4,2]
+R
