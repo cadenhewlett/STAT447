4ca5e15677b3c7b770a70131ecc0e9bae1081954
Caden Hewlett <cadenh139@edu.sd45.bc.ca>
2024-03-10 22:42:16 -0700

    full process data translation is done

--- floor_plan_translator.py
+++ floor_plan_translator.py
@@ -19,11 +19,38 @@ image_resized = original_image # cv2.resize(image, (width, height))
 useless, image_thresh = cv2.threshold(original_image, 127, 255, cv2.THRESH_BINARY)
 
 # define the grid size (m x n)
-m, n = 15, 45  
+m, n = 22, 45  
 
 # get the size of each cell in the grid
 cell_height = height // m
 cell_width = width // n
+
+matrix = np.zeros((m, n), dtype=int)
+# Analyze each cell in the grid
+for i in range(m):
+    for j in range(n):
+        # Extract the cell from the thresholded image
+        cell = image_thresh[i*cell_height:(i+1)*cell_height, j*cell_width:(j+1)*cell_width]
+        # Determine if the cell contains any black or grey (value < 255)
+        if np.any(cell < 255):
+            matrix[i, j] = 1
+
+# Create a copy of the original resized image for drawing
+annotated_image = cv2.cvtColor(image_resized, cv2.COLOR_GRAY2BGR)
+
+# Draw the grid and annotate
+for i in range(m):
+    for j in range(n):
+        top_left = (j*cell_width, i*cell_height)
+        bottom_right = ((j+1)*cell_width, (i+1)*cell_height)
+        cv2.rectangle(annotated_image, top_left, bottom_right, (0, 0, 0), 1)
+        if matrix[i, j] == 1:
+            col_printed = (255, 0, 0)
+        else: col_printed = (0, 0, 255)
+        text_position = (top_left[0] + 5, top_left[1] + 15)
+        cv2.putText(annotated_image, str(matrix[i, j]), 
+                    text_position, cv2.FONT_HERSHEY_SIMPLEX, 0.4, col_printed, 1)
+
 
 # draw grid lines
 for i in range(0, width, cell_width):
@@ -31,24 +58,31 @@ for i in range(0, width, cell_width):
 for j in range(0, height, cell_height):
     cv2.line(image_thresh, (0, j), (width, j), color=(0), thickness=1)
 
+
 # plot the images
-fig, ax = plt.subplots(1, 3, figsize=(24, 8))  
+fig, ax = plt.subplots(2, 2, figsize=(30, 12))  
 
 # pre-processed (original) floor plan
-ax[0].imshow(pre_processed_image, cmap='gray')
-ax[0].title.set_text('Pre-Processed Floor Plan')
-ax[0].axis('off')  
+ax[0,0].imshow(pre_processed_image, cmap='gray')
+ax[0,0].title.set_text('Pre-Processed Floor Plan')
+ax[0,0].axis('off')  
 
 # original floor plan (with label edits)
-ax[1].imshow(original_image, cmap='gray')
-ax[1].title.set_text('Processed Floor Plan')
-ax[1].axis('off')  
+ax[0,1].imshow(original_image, cmap='gray')
+ax[0,1].title.set_text('Processed Floor Plan')
+ax[0,1].axis('off')  
 
 # with the grid overlay
-ax[2].imshow(image_thresh, cmap='gray')
-ax[2].title.set_text('Floor Plan with Grid Overlay')
-ax[2].axis('off')  
+ax[1,0].imshow(image_thresh, cmap='gray')
+ax[1,0].title.set_text('Floor Plan with Grid Overlay')
+ax[1,0].axis('off')  
 
+# with annotation
+ax[1, 1].imshow(annotated_image, aspect='auto')
+ax[1, 1].set_title('Floor Plan Matrix with Annotations')
+ax[1, 1].axis('off')
+
+plt.subplots_adjust(wspace=0.05, hspace=0.005)
 plt.show()
 
 
