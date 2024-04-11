c3dda4d167494e1fe6279504eec7cf6e8143f797
Caden Hewlett <cadenh139@edu.sd45.bc.ca>
2024-04-10 20:05:04 -0700

    data and processing (too much data in source)

---
+++ final_project/data_processing.R
@@ -0,0 +1,38 @@
+library(dplyr)
+library(ggplot2)
+# read data
+df = read.csv("final_project/SelfRef_Maths_Exp1.csv")
+
+# randomization seed 
+set.seed(713)
+
+# we used a subset of 75 children who answered the question wrong
+# and in each case inspected the length of the question. 
+# in interest of classification, we separated them into 4 categories dictating
+# the question length
+wrong = df[df$ResponseCode == 0 & nchar(df$QuestionText)>0, ]
+i = sample(1:nrow(wrong), 75)
+subdf = wrong[i, ]
+# get lengths of question
+lengths = as.numeric(sapply(subdf$QuestionText, nchar))
+# time to answer question, in miliseconds
+time = subdf$ResponseRT
+# categorization of question length
+category <- case_when(
+  lengths <= 75 ~ "short",
+  lengths <= 90 ~ "average",
+  lengths <= 100 ~ "long",
+  TRUE ~ "very long"  
+)
+# out df for model
+out_df = data.frame(
+  K = category, 
+  # convert to seconds
+  Y = time*0.001,
+  X = lengths
+)
+# plotting to make sure it's all working
+print( ggplot(out_df, aes(x = X, y = Y, col = K)) + geom_point() + theme_bw() )
+
+# write to CSV
+write.csv( out_df, file = "main_data.csv", row.names = FALSE)
--- final_project/main.R
+++ final_project/main.R
@@ -1,2 +1,48 @@
-getwd()
-install.packages("final_project/DPpackage_1.1-7.tar.gz", repos = NULL, type = "source")
+library(extraDistr)
+library(ggplot2)
+
+
+# base distributions, 
+G_0_nor <- function(n) { rnorm(n, 0, 4) }
+G_0_dir <- function(n) { rdirichlet(n, c(1, 1, 1, 1)) } 
+# clusters 
+n <- 1000
+# generate stick breaking finite approximation
+b <- rbeta(n, 1, 10) 
+# empty vector for pulls
+p <- numeric(n)
+# break 1
+p[1] <- b[1]
+# further breaks
+p[2:n] <- sapply(2:n, function(i) b[i] * prod(1 - b[1:(i-1)]))
+
+# create vector
+y <- cbind(G_0_nor(n),  G_0_dir(n)) 
+# index sample according to stick-break probabilities
+sampled <- sample(1:n, prob = p, replace = TRUE)
+# this creates theta_i
+theta = y[sampled,]
+
+
+# take log-product of dirichlett component so that it can be visualized
+plotDF = data.frame( B = theta[,1],  
+                     P = log(apply(theta[, 2:5], 1, prod)))
+
+# plot heatmap of results
+plot = ggplot(plotDF, aes(x = B, y = P)) +
+  geom_density_2d_filled() +
+  labs(title = "Dirichlet Process",
+       y = "Log-Product Dirichlet Base",
+       x = "Normal Base") + theme_bw()
+
+print(plot)
+
+unique(plotDF)
+# DP generates the 2D-distribution P, where one dimension of P is
+# normal N(m, s) and the other dimension is dirchlet
+
+# then, x_{i,j} are sampled from the dirichlet dimension of P 
+# and, betas are sampled from the normal dimension of p
+# all according to the probability mass defined by p (i.e. stick-breaking)
+# then, Y_i | (all of that) is the poisson fit of the i-th data point
+# by the indicator-based regression equation provided?
