b9bbd51d141cb2c20c2acd95554c49cf2292133d
Caden Hewlett <cadenh139@edu.sd45.bc.ca>
2024-04-11 09:23:44 -0700

    palette enrichment

--- final_project/main.R
+++ final_project/main.R
@@ -35,10 +35,12 @@ palette_3 = c("#fcf2d7", "#FFBA08", "#FAA307", "#F48C06",
                "#9D0208", "#6A040F", "#370617", "#03071E")
 palette_2 <- c("#d8f3dc", "#b7e4c7", "#a6ddb8", "#95d5b2", "#74c69d", "#52b788", 
                "#49a47a", "#40916c", "#2d6a4f", "#1b4332", "#081c15")
-palette_1 <- c("#d9ed92", "#b5e48c", "#99d98c", "#76c893", "#52b69a", 
-                   "#34a0a4", "#168aad", "#1a759f", "#1e6091", "#184e77", 
-                   "#15395d")
-# + scale_fill_manual(values = rev(palette_4))
+palette_1 <- c("#D9ED92", "#BDE68D", "#A6DE8C", "#8ED48E" ,
+               "#73C794", "#58B999", "#40A8A0", "#2898A7", "#1787AB",
+               "#1A77A0", "#1D6695", "#1B5885",
+               "#174971" ,"#15395D")
+
+# +  scale_fill_viridis_d(option = "plasma") 
 plotDF = data.frame(  DirB = theta,  DirP = log(p))
 # plot heatmap of results
 p1 = ggplot(plotDF, aes(x = DirB, y = DirP)) +
@@ -49,7 +51,7 @@ p1 = ggplot(plotDF, aes(x = DirB, y = DirP)) +
     subtitle = TeX("Where $K = 1000$ and $G_0 \\sim N(0, \\sigma^2)$"),
     y = TeX("Log of Mixture Weights: $\\{\\pi_k\\}_{k = 1}^K$"),
     x = TeX("Cluster Parameters: $\\{\\theta_k\\}_{k = 1}^K$")
-  )  + theme_bw()  +  scale_fill_viridis_d(option = "plasma") + theme(
+  )  + theme_bw()  +  scale_fill_manual(values = rev(palette_4_enriched)) + theme(
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     panel.border = element_blank(),
@@ -66,3 +68,25 @@ p1 = ggplot(plotDF, aes(x = DirB, y = DirP)) +
 print(p1) #trbl
 
 ggsave("final_project/dirch_appx.png", plot = p1, width = 7, height = 5)
+
+
+palette_1 <- c("#d9ed92", "#b5e48c", "#99d98c", "#76c893", "#52b69a", 
+               "#34a0a4", "#168aad", "#1a759f", "#1e6091", "#184e77", 
+               "#15395d")
+
+# Function to interpolate colors
+interpolate_colors <- function(colors, n) {
+  cols <- grDevices::col2rgb(colors) / 255
+  t <- seq(0, 1, length.out = length(colors))
+  t_new <- seq(0, 1, length.out = n)
+  r <- approx(t, cols[1,], t_new)$y
+  g <- approx(t, cols[2,], t_new)$y
+  b <- approx(t, cols[3,], t_new)$y
+  rgb(r, g, b, maxColorValue = 1)
+}
+
+# Generate the enriched palette
+palette_4_enriched <- interpolate_colors(palette_4, 14)
+
+# Print the enriched palette
+palette_1_enriched
